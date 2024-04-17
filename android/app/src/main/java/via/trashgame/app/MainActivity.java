3486556e15251028d1b4736653097b0fa6fa6c3c
Matas Armonaitis <matas.armonaitis@gmail.com>
2024-04-17 09:21:00 +0200

    coke

---
+++ android/app/src/main/java/via/trashgame/app/MainActivity.java
@@ -0,0 +1,5 @@
+package via.trashgame.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ app/data.ts
@@ -0,0 +1,17 @@
+const toilet = {
+    humanWaste : "human waste",
+    toiletPaper : "toilet paper"
+}
+const notToilet = {
+    wetWipes : "wet wipes",
+    paperTowels : "paper towels",
+    feminineHygieneProducts : "feminine hygiene products",
+    dentalFloss : "dental floss",
+    diapers : "diapers",
+    hair : "hair",
+    cottonBallsAndSwabs : "cotton balls and swabs",
+    medications : "medications",
+    cookingGreaseAndOil : "cooking grease and oil",
+    foodScraps : "food scraps",
+    catLitter : "cat litter",
+}
