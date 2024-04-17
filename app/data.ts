2b415e49e1cdac4abbe832dfad53a3313e79d2a8
Matas Armonaitis <matas.armonaitis@gmail.com>
2024-04-17 11:27:05 +0200

    Added rizz

--- app/data.ts
+++ app/data.ts
@@ -1,17 +1,23 @@
-export const toilet: Array<string> = [
-    "human waste",
-    "toilet paper"
+export type DataType = {
+    name: string;
+    image: string;
+};
+
+export const toilet: DataType[] = [
+    { name: "human waste", image: "../images/poop.png" },
+    { name: "toilet paper", image: "../images/toilet-paper.png" },
 ];
-export const notToilet: Array<string> = [
-    "wet wipes",
-    "paper towels",
-    "feminine hygiene products",
-    "dental floss",
-    "diapers",
-    "hair",
-    "cotton balls and swabs",
-    "medications",
-    "cooking grease and oil",
-    "food scraps",
-    "cat litter",
+
+export const notToilet: DataType[] = [
+    { name: "wet wipes", image: "/images/wet-wipes.png" },
+    { name: "paper towels", image: "./images/paper-roll.png" },
+    { name: "feminine hygiene products", image: "../images/tampon.png" },
+    { name: "dental floss", image: "../images/dental-floss.png" },
+    { name: "diapers", image: "../images/diapers.png" },
+    { name: "hair", image: "../images/hair.png" },
+    { name: "cotton balls and swabs", image: "../images/cotton-buds.png" },
+    { name: "medications", image: "../images/pills.png" },
+    { name: "cooking grease and oil", image: "../images/olive-oil.png" },
+    { name: "food scraps", image: "../images/street-food.png" },
+    { name: "cat litter", image: "../images/catLitter.png" },
 ];
--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,21 @@
-'use client'
-import { useEffect, useState } from 'react';
-import { useSwipeable } from 'react-swipeable';
-import { notToilet, toilet } from './data';
+"use client";
+import { useEffect, useState } from "react";
+import { useSwipeable } from "react-swipeable";
+import { DataType, notToilet, toilet } from "./data";
 // @ts-ignore
-import useSound from 'use-sound';
+import useSound from "use-sound";
 // @ts-ignore
-import ding from '../sounds/ding.mp3';
+import ding from "../sounds/ding.mp3";
 // @ts-ignore
-import bruh from '../sounds/bruh.mp3';
-import toiletImage from '../images/spiningToilet.gif';
-import trashCanImage from '../images/trashCan.gif';
+import bruh from "../sounds/bruh.mp3";
+import rizz from "../sounds/rizz.mp3";
+import toiletImage from "../public/images/spiningToilet.gif";
+import trashCanImage from "../public/images/trashCan.gif";
 
 
-
-
-const toiletItems: string[] = Object.values(toilet);
-const notToiletItems: string[] = Object.values(notToilet);
-const allItems: string[] = [...toiletItems, ...notToiletItems];
-
+const toiletItems: DataType[] = toilet;
+const notToiletItems: DataType[] = notToilet;
+const allItems: DataType[] = [...toiletItems, ...notToiletItems];
 
 export default function Home() {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -25,10 +23,11 @@ export default function Home() {
   const [isCorrect, setIsCorrect] = useState(false);
   const [highScore, setHighScore] = useState(0);
 
-
   const [playDing] = useSound(ding);
   const [playBruh] = useSound(bruh);
+  const [playRizz] = useSound(rizz);
 
+  console.log("trashcanImage", trashCanImage)
 
   useEffect(() => {
     setCurrentIndex(Math.floor(Math.random() * allItems.length));
@@ -37,19 +36,23 @@ export default function Home() {
   useEffect(() => {
     if (score > Number(highScore)) {
       setHighScore(score);
-      if (typeof window !== 'undefined') {
-        localStorage.setItem('highScore', score.toString());
+      if (typeof window !== "undefined") {
+        localStorage.setItem("highScore", score.toString());
       }
     }
   }, [score]);
 
   useEffect(() => {
-  if (typeof window !== 'undefined') {
-    setHighScore(Number(localStorage.getItem('highScore')) || 0);
-  }
-}, []);
+    if (typeof window !== "undefined") {
+      setHighScore(Number(localStorage.getItem("highScore")) || 0);
+    }
+  }, []);
 
-
+  useEffect(() => {
+    if (score === 10) {
+      playRizz();
+    }
+  }, [score]);
 
   const handlers = useSwipeable({
     //Swiped left so to the toilet
@@ -62,15 +65,16 @@ export default function Home() {
   const goToNextTrash = (swipedRight: boolean) => {
     const currentItem = allItems[currentIndex];
     const isToiletItem = toiletItems.includes(currentItem);
-    const wasCorrect = (!swipedRight && isToiletItem) || (swipedRight && !isToiletItem);
+    const wasCorrect =
+      (!swipedRight && isToiletItem) || (swipedRight && !isToiletItem);
 
     setIsCorrect(wasCorrect);
 
-    console.log("swiped right: ", swipedRight)
-    console.log("item was: ", currentItem, "was correct: ", wasCorrect)
+    console.log("swiped right: ", swipedRight);
+    console.log("item was: ", currentItem, "was correct: ", wasCorrect);
 
     if (wasCorrect) {
-      setScore(prevScore => prevScore + 1);
+      setScore((prevScore) => prevScore + 1);
       playDing();
     } else {
       setScore(0);
@@ -85,18 +89,54 @@ export default function Home() {
     <div {...handlers} className="game-container">
       <h1>Trash Game</h1>
       <h2>High Score: {highScore}</h2>
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <img className='toiletImage' src={toiletImage.src} alt="Toilet" />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <img className="toiletImage" src={toiletImage.src} alt="Toilet" />
 
-        <div className={`trash-item ${isCorrect ? 'correctGuess' : 'wrongGuess'}`} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
-          {allItems[currentIndex]}
+        <div
+          className={`trash-item ${isCorrect ? "correctGuess" : "wrongGuess"}`}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          {allItems[currentIndex].name}
+          <img
+            className="itemIcon"
+            src={allItems[currentIndex].image}
+            alt="icon for item"
+            style={{
+              width: "40px",
+              height: "40px",
+              objectFit: "cover",
+            }}
+          />
         </div>
-        <img className='trashCanImage' src={trashCanImage.src} alt="Trash can" />
-
+        <img
+          className="trashCanImage"
+          src={trashCanImage.src}
+          alt="Trash can"
+        />
       </div>
-      <div className="score" style={{ animation: isCorrect ? 'score-increase 1s' : '' }}>
-        Score: {score > 10 ? '🔥' : ''} {score} {score > 10 ? '🔥' : ''}
+      <div
+        className="score"
+        style={{ animation: isCorrect ? "score-increase 1s" : "" }}
+      >
+        Score: {score >= 10 ? "🔥" : ""} {score} {score >= 10 ? "🔥" : ""}
       </div>
+      <p>
+        Icons by :{" "}
+        <a target="_blank" href="https://icons8.com">
+          Icons8
+        </a>
+      </p>
     </div>
   );
 }
