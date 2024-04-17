c319ab3091f4c1fdc1831d02435c3e2028011b94
Matas Armonaitis <matas.armonaitis@gmail.com>
2024-04-17 11:33:45 +0200

    paper towels change

--- app/data.ts
+++ app/data.ts
@@ -6,11 +6,12 @@ export type DataType = {
 export const toilet: DataType[] = [
     { name: "human waste", image: "../images/poop.png" },
     { name: "toilet paper", image: "../images/toilet-paper.png" },
+    { name: "paper towels", image: "./images/paper-roll.png" },
+
 ];
 
 export const notToilet: DataType[] = [
     { name: "wet wipes", image: "/images/wet-wipes.png" },
-    { name: "paper towels", image: "./images/paper-roll.png" },
     { name: "feminine hygiene products", image: "../images/tampon.png" },
     { name: "dental floss", image: "../images/dental-floss.png" },
     { name: "diapers", image: "../images/diapers.png" },
